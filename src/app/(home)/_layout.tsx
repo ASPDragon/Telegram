38d1c4ab0db9997f7ce111bcc8fec83e957bcab0
Sergei Avdoshkin <iocaster@Sergeis-MacBook-Air.local>
2024-05-07 10:52:04 +0800

    Stream Chat Provider

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,39 +1,12 @@
 import { Stack } from 'expo-router';
-import { StreamChat } from 'stream-chat';
-import { Chat, OverlayProvider } from 'stream-chat-expo';
-import { useEffect } from "react";
-
-const client = StreamChat.getInstance('fx9k3vdnaqf8');
+import ChatProvider from "../../providers/ChatProvider";
 
 export default function HomeLayout() {
-    useEffect(() => {
-        const connect = async () => {
-            await client.connectUser(
-                {
-                    id: 'jlahey',
-                    name: 'Jim Lahey',
-                    image: 'https://i.imgur.com/fR9Jz14.png',
-                },
-                client.devToken('jlahey')
-            );
-
-            // const channel = client.channel('messaging', 'the_park', {
-            //     name: 'The Park',
-            // });
-            //
-            // await channel.watch();
-        };
-
-        connect();
-    });
-
     return (
-        <OverlayProvider>
-            <Chat client={client}>
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                </Stack>
-            </Chat>
-        </OverlayProvider>
+        <ChatProvider>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </ChatProvider>
     );
 }
