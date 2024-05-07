60d6bfe5bb7728c4900a8f8e966999d3aac71f5b
Sergei Avdoshkin <iocaster@Sergeis-MacBook-Air.local>
2024-05-07 15:49:28 +0800

    Connect supabase user to streamchat user

--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -2,21 +2,27 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { StreamChat } from 'stream-chat';
 import { Chat, OverlayProvider } from 'stream-chat-expo';
 import { ActivityIndicator, View } from "react-native";
+import { useAuth } from "./AuthProvider";
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 export default function ChatProvider({children}: PropsWithChildren) {
     const [isReady, setIsReady] = useState(false);
+    const { profile } = useAuth();
 
     useEffect(() => {
+        if (!profile) {
+            return;
+        }
+
         const connect = async () => {
             await client.connectUser(
                 {
-                    id: 'jlahey',
-                    name: 'Jim Lahey',
+                    id: profile.id,
+                    name: profile.full_name,
                     image: 'https://i.imgur.com/fR9Jz14.png',
                 },
-                client.devToken('jlahey')
+                client.devToken(profile.id)
             );
 
             setIsReady(true);
@@ -31,10 +37,12 @@ export default function ChatProvider({children}: PropsWithChildren) {
         connect();
 
         return () => {
-            client.disconnectUser();
+            if (isReady) {
+                client.disconnectUser();
+            }
             setIsReady(false);
         };
-    }, []);
+    }, [profile?.id]);
 
     if (!isReady) {
         return (
